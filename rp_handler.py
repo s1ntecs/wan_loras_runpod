eeccebd3767db27d00f2266524c430610061aa99
sintecs <s1ntecs@icloud.com>
2025-06-03 22:40:22 +0500

    first commit

--- rp_handler.py
+++ rp_handler.py
@@ -261,6 +261,12 @@ class Predictor():
 # -------------------------------------------------------------
 logger = RunPodLogger()
 
+if torch.cuda.is_available():
+    print("Current CUDA device:", torch.cuda.current_device())
+    print("Device name:", torch.cuda.get_device_name(torch.cuda.current_device()))
+    predictor = Predictor()
+    predictor.setup()
+
 
 def handler(job):
     global predictor
